b4a746192f4234f5ccd3825158376f22d3063ea4
Jazz Jiang <526446847@qq.com>
2019-04-01 16:00:16 +0800

    add chat

---
+++ client/pages/chat.vue
@@ -0,0 +1,10 @@
+/*
+ * @Author: Janzen 
+ * @Date: 2019-04-01 14:27:15 
+ * @Last Modified by: Janzen
+ * @Last Modified time: 2019-04-01 14:28:02
+ */
+<script>
+  import Chat from '@/pages/_lang/chat'
+  export default Chat
+</script>
---
+++ client/pages/chat/index.vue
@@ -0,0 +1,10 @@
+/*
+ * @Author: Janzen 
+ * @Date: 2019-04-01 14:27:15 
+ * @Last Modified by: Janzen
+ * @Last Modified time: 2019-04-01 14:27:42
+ */
+<script>
+  import ChatIndex from '@/pages/_lang/chat/index'
+  export default ChatIndex
+</script>
