16919dc25ecf7f4a15d2a6e5dfa92c3ef4ec325e
Jazz Jiang <526446847@qq.com>
2019-04-04 15:41:45 +0800

    foot nav 状态修正

--- client/components/common/Footer.vue
+++ client/components/common/Footer.vue
@@ -2,25 +2,15 @@
  * @Author: Janzen 
  * @Date: 2019-03-04 18:51:54 
  * @Last Modified by: Janzen
- * @Last Modified time: 2019-04-04 12:57:17
+ * @Last Modified time: 2019-04-04 15:41:19
  */
 <template>
-  <v-bottom-nav :class="className" :value="true" fixed height="48px" color="white">
-    <v-btn color="teal" flat :to="`${preLink}/`" value="foryou">
-      <span>foryou</span>
-      <v-icon v-text="$vuetify.icons.home"></v-icon>
+  <div class="v-item-group v-bottom-nav theme--light v-bottom-nav--fixed v-bottom-nav--active white m-footer">
+    <v-btn v-for="item in items" :key="item.name" :class="{'v-btn--active': isActive(item)}" color="teal" flat :to="item.to" exact>
+      <span>{{ item.name }}</span>
+      <v-icon v-text="$vuetify.icons[item.icon]"></v-icon>
     </v-btn>
-
-    <v-btn color="teal" flat :to="`${preLink}/ranking`" value="ranking">
-      <span>ranking</span>
-      <v-icon v-text="$vuetify.icons.topics"></v-icon>
-    </v-btn>
-
-    <v-btn color="teal" flat :to="`${preLink}/task`" value="task">
-      <span>task</span>
-      <v-icon v-text="$vuetify.icons.user"></v-icon>
-    </v-btn>
-  </v-bottom-nav>
+  </div>
 </template>
 <script>
 import { mapState, mapGetters } from 'vuex'
@@ -33,7 +23,37 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['preLink', 'username'])
+    ...mapGetters(['preLink', 'username']),
+    // 导航数据
+    items() {
+      return [
+        { to: { path: `${this.preLink}/` }, name: 'foryou', icon: 'home' },
+        {
+          to: { path: `${this.preLink}/ranking` },
+          name: 'ranking',
+          icon: 'topics'
+        },
+        { to: { path: `${this.preLink}/task` }, name: 'task', icon: 'user' }
+      ]
+    }
+  },
+  methods: {
+    /**
+     * 当前tab是否被选中
+     * @param {obj} ite 当前数据
+     */
+    isActive(ite) {
+      let { path } = this.$route
+      let name = 'none'
+      this.items.some((item, i) => {
+        let status = item.to.path === path
+        if (status) {
+          name = item.name
+        }
+        return status
+      })
+      return name === ite.name
+    }
   }
 }
 </script>
